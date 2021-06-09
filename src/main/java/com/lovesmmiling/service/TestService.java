465668722be50352ec1a3c8ef4d10a4b161c98f0
LoveSmiling <1134603918@qq.com>
2021-06-09 11:40:42 +0800

    dev

--- src/main/java/com/lovesmmiling/service/TestService.java
+++ src/main/java/com/lovesmmiling/service/TestService.java
@@ -24,7 +24,7 @@ public class TestService {
     private RestTemplate restTemplate;
 
     @PostConstruct
-    public void getFile() {
+    public void getFile() throws IOException {
         String url = "http://api.hclyz.com:81/mf/json.txt";
         ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(null, null), String.class);
         String body = exchange.getBody();
@@ -33,41 +33,35 @@ public class TestService {
         List<Tv> tvs = JSONArray.parseArray(pingtai.toJSONString(), Tv.class);
 
         String getUrl = "";
+        long startTime = System.currentTimeMillis();
+        OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream("tv.dpl"), "UTF-8");
+        String s = "DAUMPLAYLIST\n" +
+                "playname = \n" +
+                "topindex = 0\n";
+        out.write(s, 0, s.length());
+        int i = 1;
         for (Tv tv : tvs) {
             getUrl = "http://api.hclyz.com:81/mf/" + tv.getAddress();
-            long st = System.currentTimeMillis();
             ResponseEntity<String> ex = restTemplate.exchange(getUrl, HttpMethod.GET, new HttpEntity<>(null, null), String.class);
-            long et = System.currentTimeMillis();
-            long l = et - st;
-            System.out.println("qingqiu" + l);
             String bodys = ex.getBody();
             JSONObject jsonObjects = JSONObject.parseObject(bodys);
             JSONArray zhubo = jsonObjects.getJSONArray("zhubo");
             List<Zhubo> zhubos = JSONArray.parseArray(zhubo.toJSONString(), Zhubo.class);
-
             StringBuilder stringBuilder = new StringBuilder();
-            int i = 1;
             try {
-                long startTime = System.currentTimeMillis();
-                String address = tv.getAddress();
-                String[] split = address.split("\\.");
-                OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(split[0] + ".dpl"), "UTF-8");
-                String s = "DAUMPLAYLIST\n" +
-                        "playname = \n" +
-                        "topindex = 0\n";
-                out.write(s, 0, s.length());
                 for (Zhubo zhubo1 : zhubos) {
-                    stringBuilder.append(i + "*file*" + zhubo1.getAddress() + "\n" + i + "*title*" + zhubo1.getTitle() + "\n" + i + "*played*0"+"\n");
+                    stringBuilder.append(i + "*file*" + zhubo1.getAddress() + "\n" + i + "*title*" + zhubo1.getTitle() + "\n" + i + "*played*0" + "\n");
                     i++;
                 }
                 out.write(stringBuilder.toString(), 0, stringBuilder.toString().length());
-                out.close();
-                long endTime = System.currentTimeMillis();
-                long time = endTime - startTime;
-                System.out.println(time);
-                System.out.println("文件创建成功！");
             } catch (IOException e) {
+
             }
         }
+        out.close();
+        long endTime = System.currentTimeMillis();
+        long l = endTime - startTime;
+        System.out.println(l);
+        System.out.println("文件创建成功！");
     }
 }
